4b31ba58a3fc65947f5a0bc3c846fb5e9c776ccc
Renato <111289175+renatostamatovski@users.noreply.github.com>
2022-10-15 22:46:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import snowflake.connector
 from urllib.error import URLError
 streamlit.title('My parent New Healthy Diner')
 
-streamlit.header('Breakfast Favorites - my first app')
+streamlit.header('Breakfast Favorites - My First App')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
